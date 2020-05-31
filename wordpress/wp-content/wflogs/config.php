41b472a09edeb8047df8ac0e9fa85d5c298aac9d
dhyey-R12 <61320689+dhyey-R12@users.noreply.github.com>
2020-05-31 21:34:27 +0530

    update

---
+++ wordpress/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1591436995;s:7:"authKey";s:64:"gu*(Ue3)?T.*ICkS`c*L])%PfqFo1Tlo3;hUhy[)VMMK#Dpyt+i%GbvY)Af(EzsX";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2766;}
